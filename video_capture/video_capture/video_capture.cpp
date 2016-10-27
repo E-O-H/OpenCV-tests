4b8984fd0b787214f97235473e94574cf9a99de5
Chenyang Tang <ct1856@nyu.edu>
2016-10-26 20:23:35 -0400

    video_capture

---
+++ video_capture/video_capture/video_capture.cpp
@@ -0,0 +1,60 @@
+// video_capture.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+//debug:
+#pragma comment(lib, "opencv_calib3d2413d.lib")
+#pragma comment(lib, "opencv_contrib2413d.lib")
+#pragma comment(lib, "opencv_core2413d.lib")
+#pragma comment(lib, "opencv_features2d2413d.lib")
+#pragma comment(lib, "opencv_flann2413d.lib")
+#pragma comment(lib, "opencv_gpu2413d.lib")
+#pragma comment(lib, "opencv_highgui2413d.lib")
+#pragma comment(lib, "opencv_imgproc2413d.lib")
+#pragma comment(lib, "opencv_legacy2413d.lib")
+#pragma comment(lib, "opencv_ml2413d.lib")
+#pragma comment(lib, "opencv_nonfree2413d.lib")
+#pragma comment(lib, "opencv_objdetect2413d.lib")
+#pragma comment(lib, "opencv_ocl2413d.lib")
+#pragma comment(lib, "opencv_photo2413d.lib")
+#pragma comment(lib, "opencv_stitching2413d.lib")
+#pragma comment(lib, "opencv_superres2413d.lib")
+#pragma comment(lib, "opencv_ts2413d.lib")
+#pragma comment(lib, "opencv_video2413d.lib")
+#pragma comment(lib, "opencv_videostab2413d.lib")
+
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+
+int main(int argc, char** argv) {
+	VideoCapture cap;
+	// open the default camera, use something different from 0 otherwise;
+	// Check VideoCapture documentation.
+	if (!cap.open(0))
+		return 0;
+
+	// text to display
+	string text;
+	int pos_x, pos_y;
+	std::cin >> text >> pos_x >> pos_y;
+
+	while (true) {
+		Mat frame;
+		cap >> frame;
+		if (frame.empty()) break; // end of video stream
+		cv::putText(frame,
+			text.c_str(),
+			cv::Point(pos_x, pos_y), // Coordinates
+			cv::FONT_HERSHEY_COMPLEX_SMALL, // Font
+			2.0, // Scale 2x bigger
+			cv::Scalar(255, 255, 255), // Color
+			1, // Thickness
+			CV_AA); // Anti-alias
+		imshow("video title", frame);
+		if (waitKey(1) == 27) break; // stop capturing by pressing ESC 
+	}
+	return 0;
+}
+
