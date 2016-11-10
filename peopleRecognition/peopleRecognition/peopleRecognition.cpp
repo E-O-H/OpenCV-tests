0f633b54c92cba5131190af5799009811ae92981
Chenyang Tang <ct1856@nyu.edu>
2016-11-10 00:09:00 -0500

    PeopleRecognition

---
+++ peopleRecognition/peopleRecognition/peopleRecognition.cpp
@@ -0,0 +1,105 @@
+// peopleRecognition.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+
+#define DEBUG  // Comment this line if building release version.
+
+#ifdef DEBUG
+//debug:
+#pragma comment(lib, "opencv_calib3d2413d.lib")
+#pragma comment(lib, "opencv_contrib2413d.lib")
+#pragma comment(lib, "opencv_core2413d.lib")
+#pragma comment(lib, "opencv_features2d2413d.lib")
+#pragma comment(lib, "opencv_flann2413d.lib")
+#pragma comment(lib, "opencv_gpu2413d.lib")
+#pragma comment(lib, "opencv_highgui2413d.lib")
+#pragma comment(lib, "opencv_imgproc2413d.lib")
+#pragma comment(lib, "opencv_legacy2413d.lib")
+#pragma comment(lib, "opencv_ml2413d.lib")
+#pragma comment(lib, "opencv_nonfree2413d.lib")
+#pragma comment(lib, "opencv_objdetect2413d.lib")
+#pragma comment(lib, "opencv_ocl2413d.lib")
+#pragma comment(lib, "opencv_photo2413d.lib")
+#pragma comment(lib, "opencv_stitching2413d.lib")
+#pragma comment(lib, "opencv_superres2413d.lib")
+#pragma comment(lib, "opencv_ts2413d.lib")
+#pragma comment(lib, "opencv_video2413d.lib")
+#pragma comment(lib, "opencv_videostab2413d.lib")
+#endif
+#ifndef DEBUG
+//release:
+#pragma comment(lib, "opencv_calib3d2413.lib")
+#pragma comment(lib, "opencv_contrib2413.lib")
+#pragma comment(lib, "opencv_core2413.lib")
+#pragma comment(lib, "opencv_features2d2413.lib")
+#pragma comment(lib, "opencv_flann2413.lib")
+#pragma comment(lib, "opencv_gpu2413.lib")
+#pragma comment(lib, "opencv_highgui2413.lib")
+#pragma comment(lib, "opencv_imgproc2413.lib")
+#pragma comment(lib, "opencv_legacy2413.lib")
+#pragma comment(lib, "opencv_ml2413.lib")
+#pragma comment(lib, "opencv_nonfree2413.lib")
+#pragma comment(lib, "opencv_objdetect2413.lib")
+#pragma comment(lib, "opencv_ocl2413.lib")
+#pragma comment(lib, "opencv_photo2413.lib")
+#pragma comment(lib, "opencv_stitching2413.lib")
+#pragma comment(lib, "opencv_superres2413.lib")
+#pragma comment(lib, "opencv_ts2413.lib")
+#pragma comment(lib, "opencv_video2413.lib")
+#pragma comment(lib, "opencv_videostab2413.lib")
+#endif
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[])
+{
+	VideoCapture cap(CV_CAP_ANY);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+	if (!cap.isOpened())
+		return -1;
+
+	Mat img;
+	HOGDescriptor hog;
+	hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+	namedWindow("video capture", CV_WINDOW_AUTOSIZE);
+	while (true)
+	{
+		cap >> img;
+		if (!img.data)
+			continue;
+
+		vector<Rect> found, found_filtered;
+		hog.detectMultiScale(img, found, 0, Size(8, 8), Size(32, 32), 1.05, 2);
+
+		size_t i, j;
+		for (i = 0; i<found.size(); i++)
+		{
+			Rect r = found[i];
+			for (j = 0; j<found.size(); j++)
+				if (j != i && (r & found[j]) == r)
+					break;
+			if (j == found.size())
+				found_filtered.push_back(r);
+		}
+		for (i = 0; i<found_filtered.size(); i++)
+		{
+			Rect r = found_filtered[i];
+			r.x += cvRound(r.width*0.1);
+			r.width = cvRound(r.width*0.8);
+			r.y += cvRound(r.height*0.06);
+			r.height = cvRound(r.height*0.9);
+			rectangle(img, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
+		}
+		imshow("video capture", img);
+		if (waitKey(20) >= 0)
+			break;
+	}
+	return 0;
+}
+
